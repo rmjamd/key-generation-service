4a87d0bc143de0460463ca6f1ca4914b31e7d722
Ramij <ramijnalpur@gmail.com>
2023-08-11 09:29:50 +0530

    First Commit

---
+++ kgs-core/src/main/java/com/ramij/kgs/core/Sample.java
@@ -0,0 +1,7 @@
+package com.ramij.kgs.core;
+
+public class Sample {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ kgs-server/src/main/java/com/ramij/kgs/server/Sample.java
@@ -0,0 +1,7 @@
+package com.ramij.kgs.server;
+
+public class Sample {
+    public static void main(String[] args) {
+
+    }
+}

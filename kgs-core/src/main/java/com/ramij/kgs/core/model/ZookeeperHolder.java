65df8cf244b16290ac1532860902625c4a9a4fd5
Ramij <ramijnalpur@gmail.com>
2023-08-22 23:20:49 +0530

    Adding File to save and retrieve File

--- kgs-core/src/main/java/com/ramij/kgs/core/model/ZookeeperHolder.java
+++ kgs-core/src/main/java/com/ramij/kgs/core/model/ZookeeperHolder.java
@@ -1,22 +1,29 @@
 package com.ramij.kgs.core.model;
 
 import com.ramij.kgs.core.exceptions.InitializationFailedException;
+import lombok.extern.log4j.Log4j2;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.retry.ExponentialBackoffRetry;
 import org.apache.zookeeper.CreateMode;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 
+@Log4j2
 public class ZookeeperHolder {
     private static final int SESSION_TIMEOUT = 5000;
     private static final String PARENT_NODE = "/worker_nodes";
     private final String zkAddress;
     private final String hostIp;
     private final String hostPort;
-    private int workerId =-1;
+    private int workerId = -1;
 
     public ZookeeperHolder(String zkAddress, String hostIp, String hostPort) {
+        log.info("ZookeeperHolder created with zkAddress: {}, hostIp: {}, hostPort: {}",
+                zkAddress, hostIp, hostPort);
         this.zkAddress = zkAddress;
         this.hostIp = hostIp;
         this.hostPort = hostPort;
@@ -28,11 +35,19 @@ public class ZookeeperHolder {
                 zkAddress,
                 new ExponentialBackoffRetry(1000, 3)
         );
-        curatorFramework.start();
+        try {
+            curatorFramework.start();
+            log.info("Curator Started:");
+        } catch (Exception e) {
+            // Handle the exception, e.g., log the error or perform custom actions
+            log.error("Failed to start CuratorFramework: " + e.getMessage());
+        }
+
         try {
             if (curatorFramework.checkExists().forPath(PARENT_NODE) == null) {
                 curatorFramework.create().forPath(PARENT_NODE);
             }
+            log.info("Parent Node  created:");
             List<String> existingNodes = curatorFramework.getChildren().forPath(PARENT_NODE);
 
             String sequentialNodePath = null;
@@ -63,7 +78,25 @@ public class ZookeeperHolder {
             throw new InitializationFailedException(e.getMessage());
         }
     }
+    private int retrieveWorkerIdFromFile() {
+        if (Files.exists(Paths.get("workerId.txt"))) {
+            try {
+                String savedWorkerId = Files.readAllLines(Paths.get("workerId.txt")).get(0);
+                return Integer.parseInt(savedWorkerId);
+            } catch (IOException e) {
+                log.error("Error reading workerId.txt: {}", e.getMessage());
+            }
+        }
+        return -1; // Return -1 if file doesn't exist or there's an error
+    }
 
+    private void saveWorkerIdToFile(int workerId) {
+        try {
+            Files.write(Paths.get("workerId.txt"), String.valueOf(workerId).getBytes());
+        } catch (IOException e) {
+            log.error("Error saving workerId to file: {}", e.getMessage());
+        }
+    }
     public int getWorkerId() {
         return workerId;
     }
--- kgs-core/src/main/java/com/ramij/kgs/core/service/impl/SnowflakeWorkerIdGenImpl.java
+++ kgs-core/src/main/java/com/ramij/kgs/core/service/impl/SnowflakeWorkerIdGenImpl.java
@@ -6,6 +6,7 @@ import com.ramij.kgs.core.exceptions.InitializationFailedException;
 import com.ramij.kgs.core.model.ServerInfo;
 import com.ramij.kgs.core.model.ZookeeperHolder;
 import com.ramij.kgs.core.service.WorkerIdGen;
+import com.ramij.kgs.core.utils.Tools;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -25,11 +26,11 @@ public class SnowflakeWorkerIdGenImpl implements WorkerIdGen {
     public SnowflakeWorkerIdGenImpl(Configurator config, ServerInfoProvider serverInfoProvider) {
         this.config = config;
         this.serverInfoProvider = serverInfoProvider;
-        ServerInfo serverInfo=serverInfoProvider.getServerDetails();
+        ServerInfo serverInfo = serverInfoProvider.getServerDetails();
         log.info("Server Details:{}", serverInfo);
         ZookeeperHolder zookeeperHolder = null;
         try {
-            zookeeperHolder = new ZookeeperHolder(serverInfo.getIpAddress(), serverInfo.getPort(), config.getZkPort());
+            zookeeperHolder = new ZookeeperHolder(Tools.getFullAddress(config.getZkHost(), config.getZkPort()), serverInfo.getIpAddress(), serverInfo.getPort());
         } catch (InitializationFailedException ex) {
             log.error("Error Occurred while Initializing Zookeeper Holder!");
         }
@@ -38,10 +39,10 @@ public class SnowflakeWorkerIdGenImpl implements WorkerIdGen {
 
     @Override
     public int getWorkerId() {
-        if(holder==null){
-            return  new Random().nextInt(1024);
+        if (holder == null) {
+            return new Random().nextInt(1024);
         }
-        log.info("Worker Id: {}",holder.getWorkerId());
+        log.info("Worker Id: {}", holder.getWorkerId());
         return holder.getWorkerId();
     }
 }

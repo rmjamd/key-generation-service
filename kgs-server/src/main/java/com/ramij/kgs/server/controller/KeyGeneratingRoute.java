d3206cd9bc7fd48e4cedb46b34408d707ab79431
Ramij <ramijnalpur@gmail.com>
2023-08-23 20:56:12 +0530

    Adding Logs to Controller

--- kgs-server/src/main/java/com/ramij/kgs/server/controller/KeyGeneratingRoute.java
+++ kgs-server/src/main/java/com/ramij/kgs/server/controller/KeyGeneratingRoute.java
@@ -1,11 +1,13 @@
 package com.ramij.kgs.server.controller;
 
 import com.ramij.kgs.server.service.KeyGenerationService;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Log4j2
 @RestController
 @RequestMapping("/kgs")
 public class KeyGeneratingRoute {
@@ -14,7 +16,12 @@ public class KeyGeneratingRoute {
 
     @GetMapping("/key")
     public String getKey() {
-        return kgs.getKey();
+        log.info("Received request to generate a key.");
+
+        String key = kgs.getKey();
+        log.info("Generated key: {}", key);
+
+        return key;
     }
 
 }

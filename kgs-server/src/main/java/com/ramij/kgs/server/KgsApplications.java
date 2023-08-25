135a96041e90bf90c1c967867c46902ba2056cc5
Ramij <ramijnalpur@gmail.com>
2023-08-25 23:05:07 +0530

    Code Refactoring

--- kgs-server/src/main/java/com/ramij/kgs/server/KgsApplications.java
+++ kgs-server/src/main/java/com/ramij/kgs/server/KgsApplications.java
@@ -3,13 +3,11 @@ package com.ramij.kgs.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication(scanBasePackages = {"com.ramij.kgs.server", "com.ramij.kgs.core"})
 public class KgsApplications {
-
     public static void main(String[] args) {
         SpringApplication.run(KgsApplications.class, args);
     }
